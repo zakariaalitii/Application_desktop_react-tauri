e4e59bd2507c799af32a10583e5ec300ad758230
Zakar <Zakar@ZAKARIA>
2022-06-26 13:48:54 +0100

    fin

---
+++ build/precache-manifest.517f011e64f6458d3041bf500b9a1e9a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcb1535dfdce8c4e18154e86026dfe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5727df9393cb4d3e0b",
+    "url": "/static/css/main.29a830d0.chunk.css"
+  },
+  {
+    "revision": "38b0d0c8ed55cbe2ba4a",
+    "url": "/static/js/2.26083098.chunk.js"
+  },
+  {
+    "revision": "cf8017249abb29042993e57dcdbf6b1d",
+    "url": "/static/js/2.26083098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5727df9393cb4d3e0b",
+    "url": "/static/js/main.ac243d2f.chunk.js"
+  },
+  {
+    "revision": "517292aa6c5393d986d0",
+    "url": "/static/js/runtime-main.ac64911a.js"
+  },
+  {
+    "revision": "cbe65582e0353ec52facecb3ab17734d",
+    "url": "/static/media/OIP.cbe65582.jpg"
+  },
+  {
+    "revision": "bb26a8c6d420676fdc0ca91ff9ab6d0f",
+    "url": "/static/media/ZakariaAliti.bb26a8c6.jpg"
+  },
+  {
+    "revision": "066ee59a3ade3e1e50c4d83055363d30",
+    "url": "/static/media/boxicons.066ee59a.woff2"
+  },
+  {
+    "revision": "85e3253de099258b06ff6703e8162322",
+    "url": "/static/media/boxicons.85e3253d.woff"
+  },
+  {
+    "revision": "ca2ad8c59b158b7a663aac4030f51e26",
+    "url": "/static/media/boxicons.ca2ad8c5.eot"
+  },
+  {
+    "revision": "ed8c2546c439f80a75d6bfb70893326e",
+    "url": "/static/media/boxicons.ed8c2546.ttf"
+  },
+  {
+    "revision": "f6e743a7c02b69102a89a57ae6bd3335",
+    "url": "/static/media/boxicons.f6e743a7.svg"
+  }
+]);
